af92a635942b543d948ef103175eb93f00b7fc44
Boris Ciobirca <boris.chobyrka@gmail.com>
2025-10-07 12:49:02 +0100

    fix: build command

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { createBullBoard } from '@bull-board/api';
+import { FastifyAdapter } from '@bull-board/fastify';
+import Fastify from 'fastify';
+import { env } from '@/env.js';
+import { createQueue, setupQueueProcessor } from '@/queue.js';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+const run = async () => {
+    const welcomeEmailQueue = createQueue('WelcomeEmailQueue');
+    await setupQueueProcessor(welcomeEmailQueue.name);
+    const server = Fastify();
+    const serverAdapter = new FastifyAdapter();
+    serverAdapter.setBasePath('/'); // **Must set before registering**
+    createBullBoard({
+        queues: [new BullMQAdapter(welcomeEmailQueue)],
+        serverAdapter,
+    });
+    server.register(serverAdapter.registerPlugin()); // no prefix/basePath needed in v5
+    server.get('/add-job', {
+        schema: {
+            querystring: {
+                type: 'object',
+                properties: {
+                    id: { type: 'string' },
+                    email: { type: 'string' },
+                },
+                required: ['id', 'email'],
+            },
+        },
+    }, (req, reply) => {
+        const { id, email } = req.query;
+        welcomeEmailQueue.add(`WelcomeEmail-${id}`, { email });
+        reply.send({ ok: true });
+    });
+    await server.listen({ port: env.PORT, host: '0.0.0.0' });
+    console.log(`Server running: http://localhost:${env.PORT}/add-job?id=1&email=hello%40world.com`);
+};
+run().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
---
+++ src/queue.js
@@ -0,0 +1,20 @@
+import { Queue, Worker } from 'bullmq';
+import { env } from '@/env.js';
+const redisURL = new URL(env.REDIS_URL);
+const connection = {
+    host: redisURL.hostname,
+    port: Number(redisURL.port),
+    username: redisURL.username,
+    password: redisURL.password,
+    family: 0,
+};
+export const createQueue = (name) => new Queue(name, { connection });
+export const setupQueueProcessor = async (queueName) => {
+    new Worker(queueName, async (job) => {
+        for (let i = 0; i <= 100; i++) {
+            await job.updateProgress(i);
+            await job.log(`Processing job at interval ${i}`);
+        }
+        return { jobId: `This is the return value of job (${job.id})` };
+    }, { connection });
+};
